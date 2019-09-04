a51c3e9312d85c1c6e38833caec62c1683f28ba7
Steven <steventruong1995@yahoo.com>
2019-09-04 14:40:21 -0400

    asked the user for their fist name

--- StringPractice/Program.cs
+++ StringPractice/Program.cs
@@ -6,8 +6,18 @@ namespace StringPractice
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your first name");
+
+            string firstName;
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Your name is: " + firstName);
+
+            Console.WriteLine("What is your last name");
+
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Your last name is: " + lastName);
         }
     }
 }
